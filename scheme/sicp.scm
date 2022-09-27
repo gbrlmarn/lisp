baeba50ba57970b3838206beed903b6ebed6071c
Gabriel Marin <gbrlmarn@gmail.com>
2022-09-27 22:52:18 +0300

    EFTA Enumerator Filter Transformer Acumulator + ex 2.33

--- scheme/sicp.scm
+++ scheme/sicp.scm
@@ -343,3 +343,116 @@
       (append rest (map (lambda (x) (cons (car items) x)) rest)))))
 (subsets x)
 
+;; 2.2.3 Sequences as Conventional Interfaces
+(define (sum-odd-squares tree)
+  (cond ((null? tree) 0)
+        ((not (pair? tree))
+         (if (odd? tree) (square tree) 0))
+        (else (+ (sum-odd-squares (car tree))
+                 (sum-odd-squares (cdr tree))))))
+                                  
+(define (even-fibs n)
+  (define (next k)
+    (if (> k n)
+      nil
+      (let ((f (fib k)))
+        (if (even? f)
+          (cons f (next (+ k 1)))
+          (next (+ k 1))))))
+  (next 0))
+
+;; filter implementation
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else
+          (filter predicate (cdr sequence)))))
+(filter odd? (list 1 2 3 4 5))
+
+;; acumulator implementation
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+    initial
+    (op (car sequence) 
+        (accumulate op initial (cdr sequence)))))
+(accumulate + 0 (list 1 2 3 4 5))
+(accumulate cons nil (list 1 2 3 4 5))
+(accumulate * 1 (list 1 2 3 4 5))
+
+;; enumerator
+(define (enumerate-interval low high)
+  (if (> low high)
+    nil
+    (cons low (enumerate-interval (+ low 1) high))))
+(enumerate-interval 1 5)
+
+;; tree enumerator
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-tree (car tree))
+                      (enumerate-tree (cdr tree))))))
+(enumerate-tree (list 1 (list 2 (list 3 4)) 5))
+
+(define x (list 1 (list 2 (list 3 4)) 5))
+
+(define (sum-odd-squares tree)
+  (accumulate +
+             0
+             (map square
+                  (filter odd?
+                          (enumerate-tree tree)))))
+(sum-odd-squares x)
+
+(define (product-of-squares-of-odd-elements sequence)
+  (accumulate *
+              1
+              (map square
+                   (filter odd? sequence))))
+(product-of-squares-of-odd-elements (list 1 2 3 4 5))
+
+;; Enumerator
+(define nil '())
+(define (enumerate-interval low high)
+  (if (> low high)
+    nil
+    (cons low (enumerate-interval (+ low 1) high))))
+(enumerate-interval 1 5)
+
+;; Filter
+(define (filter predicate items)
+  (cond ((null? items) nil)
+        ((predicate (car items))
+         (cons (car items) (filter predicate (cdr items))))
+        (else
+          (filter predicate (cdr items)))))
+(filter odd? (enumerate-interval 1 5))
+
+;; Transformator -> Map...
+
+;; Acumulator
+(define (accumulator op initial items)
+  (if (null? items)
+    initial
+    (op (car items) 
+        (accumulator op initial (cdr items)))))
+(accumulator + 0 (filter odd? (enumerate-interval 1 5)))
+
+;; --------------------------------------------------------
+;; Main takeway
+;; Programs can be reasoned about like this:
+;; Enumerator -> Filter -> map(Transformator) -> Acumulator
+;; EFTA or ETFA
+;; --------------------------------------------------------
+
+
+;; ex 2.33
+(define (my-map p items)
+  (accumulator (lambda (x y) 
+                (cons (p x) y)) 
+              nil 
+              items))
+(map square (list 1 2 3 4))
+(my-map square (list 1 2 3 4))
