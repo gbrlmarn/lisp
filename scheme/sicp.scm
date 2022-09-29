e4fb4cf1e5865beaeae591d9929a3643d4f5f346
Gabriel Marin <gbrlmarn@gmail.com>
2022-09-29 22:09:34 +0300

    2.38 & 2.39

--- scheme/sicp.scm
+++ scheme/sicp.scm
@@ -524,3 +524,48 @@
            (matrix-*-vector cols mv))
          m)))
 (matrix-*-matrix mat mat)
+
+;; ex 2.38
+(define (fold-right op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+      result
+      (op (car rest)
+          (iter result (cdr rest)))))
+  (iter initial sequence))
+
+(define (fold-left op initial sequence) 
+  (define (iter result rest)
+    (if (null? rest)
+      result
+      (iter (op result (car rest))
+            (cdr rest))))
+  (iter initial sequence))
+
+(fold-right + 0 (list 1 2 3))
+(fold-left + 0 (list 1 2 3))
+(fold-right / 1 (list 1 2 3))
+(fold-left / 1 (list 1 2 3))
+(fold-right list nil (list 1 2 3))
+;; (1 (2 (3 ())))
+(fold-left list nil (list 1 2 3))
+;; (((() 1 ) 2) 3)
+
+;; ex 2.39
+(fold-left 
+  (lambda (x y) (cons y x))
+  nil 
+  (list 1 2 3))
+(define (reverse-right sequence)
+  (fold-right (lambda (x y) 
+                (append y (list x))) 
+              nil 
+              sequence))
+(define (reverse-left sequence)
+  (fold-left (lambda (x y)
+               (cons y x))
+             nil
+             sequence))
+(reverse-right (list 1 2 3))
+(reverse-left (list 1 2 3))
+
