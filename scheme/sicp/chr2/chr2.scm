440fc2442b70ede81dce23f28a5392b7c9689e48
Gabriel Marin <gbrlmarn@gmail.com>
2022-10-15 03:03:58 +0300

    2.53-2.55

--- scheme/sicp/chr2/chr2.scm
+++ scheme/sicp/chr2/chr2.scm
@@ -783,3 +783,36 @@
 (define s (make-segment 2 5))
 (start-segment s)
 (end-segment s)
+
+;; 2.3 symbolic data
+(define (memq item x)
+  (cond ((null? x) false)
+	((eq? item (car x)) x )
+	(else (memq item (cdr x)))))
+(memq 'apple '(pear banana prune))
+(memq 'apple '(x (apple sauce) y apple pear))
+
+;; 2.53
+(list 'a 'b 'c) ;; (a b c)
+(list (list 'george)) ;; ((george))
+(cdr '((x1 x2) (y1 y2))) ;; ((y1 y2))
+(cadr '((x1 x2) (y1 y2))) ;; (y1 y2)
+(pair? (car '(a short list))) ;; #f
+(memq 'red '((red shoes) (blue socks))) ;; #f
+(memq 'red '(red shoes blue socks)) ;; (red shoes blue socks)
+
+;; 2.54
+(define (equal? a b)
+  (cond ((and (not (pair? a)) (not (pair? b))) (eq? a b))
+	((and (pair? a) (pair? b))
+	 (and (equal? (car a) (car b)) (equal? (cdr a) (cdr b))))
+	(else false)))
+(equal? '(this is a list) '(this is a list))
+(equal? '(this is a list) '(this (is a) list))
+
+;; 2.55
+(car ''abracadabra)
+;; This prints quote because '(abracadraba) can be writed as
+;; (quote abracadabra) which results that:
+;; (car '(quote abracadabra)) equals quote
+	 
