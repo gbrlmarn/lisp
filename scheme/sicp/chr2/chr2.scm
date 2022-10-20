84e6cb76beacd2511960c6416a0ce79cd7c0858c
Gabriel Marin <gbrlmarn@gmail.com>
2022-10-21 01:34:45 +0300

    2.59 and 2.60

--- scheme/sicp/chr2/chr2.scm
+++ scheme/sicp/chr2/chr2.scm
@@ -967,5 +967,46 @@
 
 (deriv '(x + 3 * (x + y + 2)) 'x)
 
+;; sets
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+	((equal? x (car set)) true)
+	(else (element-of-set? x (cdr set)))))
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+	((element-of-set? (car set1) set2)
+	 (cons (car set1)
+	       (intersection-set (cdr set1) set2)))
+	(else (intersection-set (cdr set1) set2))))
 
-       
+;; 2.59
+(define (union-set set1 set2)
+  (cond ((and (null? set1) (not (null? set2))) set2)
+	((and (null? set2) (not (null? set1))) set1)
+	((element-of-set? (car set1) set2)
+	 (union-set (cdr set1) set2))
+	(else (cons (car set1)
+		    (union-set (cdr set1) set2)))))
+
+(intersection-set (list 1 2 3) (list 2 3))
+(union-set (list 1 2) (list 3 4 5))
+
+;; 2.60
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+	((equal? x (car set)) true)
+	(else (element-of-set? x (cdr set)))))
+(define (adjoin-set x set)
+  (cons x set))
+(define (union-set set1 set2)
+  (append set1 set2))
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+	((element-of-set? (car set1) set2)
+	 (cons (car set1)
+	       (intersection-set (cdr set1) (set2))))
+	(else (intersection-set (cdr set1) (set2)))))
