1ef03c166c1b12b57c04ad154f9cfb1de00452f4
Gabriel Marin <gbrlmarn@proton.me>
2022-11-26 01:15:43 +0200

    Started chapter 3
    * scheme/sicp/chr3/chr3.scm:

---
+++ scheme/sicp/chr3/chr3.scm
@@ -0,0 +1,73 @@
+(define balance 100)
+(define (withdraw amount)
+  (if (>= balance amount)
+      (begin (set! balance (- balance amount))
+	     balance)
+      "Insufficient funds"))
+(withdraw 20)
+
+;; withdraw with internal balance
+(define new-withdraw
+  (let ((balance 100))
+    (lambda (amount)
+      (if (>= balance amount)
+	  (begin (set! balance
+		       (- balance amount))
+		 amount)
+	  "Insufficient funds"))))
+
+(define (make-withdraw balance)
+  (lambda (amount)
+    (if (>= balance amount)
+	(begin (set! balance (- balance amount))
+	       balance)
+	"Insufficient funds")))
+(define w1 (make-withdraw 100))
+(define w2 (make-withdraw 100))
+(w1 10)
+(w1 0)
+
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+	(begin (set! balance (- balance amount))
+	       balance)
+	"Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (dispatch m)
+    (cond ((eq? m 'withdraw) withdraw)
+	  ((eq? m 'deposit) deposit)
+	  (else
+	   (error "Unknown request: MAKE-ACCOUNT"
+		  m))))
+  dispatch)
+
+(define acc (make-account 100))
+((acc 'deposit) 10)
+((acc 'withdraw) 10)
+
+;; 3.1
+(define (make-accumulator start)
+  (lambda (increment)
+    (set! start (+ start increment))
+    start))
+(define A (make-accumulator 5))
+(A 10)
+
+;; 3.2
+(define (make-monitored f)
+  (define times-called 0)
+  (define (mf msg)
+    (cond ((eq? 'how-many-calls? msg) times-called)
+	  ((eq? 'reset-count msg) (set! times-called 0))
+	  (else
+	   (set! times-called (+ times-called 1))
+	   (f msg))))
+  mf)
+
+(define s (make-monitored sqrt))
+(s 100)
+(s 'how-many-calls?)
+(s 'reset-count)
