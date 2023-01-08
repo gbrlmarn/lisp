886c385be0fbf0448e41f39ec1542adfff8be7fb
Gabriel Marin <gbrlmarn@proton.me>
2023-01-08 23:48:34 +0200

    3.24

--- scheme/sicp/chr3/chr3.scm
+++ scheme/sicp/chr3/chr3.scm
@@ -869,3 +869,144 @@ w
 ((dq1 'rear-delete-deque!))
 ((dq1 'print-deque))
 
+;; Representing tables
+(define (lookup key table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+	(cdr record)
+	false)))
+(define (assoc key records)
+  (cond ((null? records) false)
+	((equal? key (caar records))
+	 (car records))
+	(else (assoc key (cdr records)))))
+(define (insert! key value table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+	(set-cdr! record value)
+	(set-cdr! table
+		  (cons (cons key value)
+			(cdr table)))))
+  'ok)
+(define (make-table)
+  (list '*table*))
+
+;; Two-dimensional tables
+(define (lookup key-1 key-2 table)
+  (let ((subtable
+	 (assoc key-1 (cdr table))))
+    (if subtable
+	(let ((record
+	       (assoc key-2 (cdr subtable))))
+	  (if records
+	      (cdr record)
+	      false))
+	false)))
+(define (insert! key-1 key-2 value table)
+  (let ((subtable (assoc
+		   key-1 (cdr table))))
+    (if  subtable
+	 (let ((record
+		(assoc key-2
+		       (cdr subtable))))
+	   (if record
+	       (set-cdr! record value)
+	       (set-cdr!
+		subtable
+		(cons (cons key-2 value)
+		      (cdr subtable)))))
+	 (set-cdr!
+	  table
+	  (cons (list key-1
+		      (cons key-2 value))
+		(cdr table)))))
+  'ok)
+
+;; table with local state
+(define false #f)
+(define (make-table)
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable
+	     (assoc key-1
+		    (cdr local-table))))
+	(if subtable
+	    (let ((record
+		   (assoc key-2
+			  (cdr subtable))))
+	      (if record
+		  (cdr record)
+		  false))
+	    false)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable
+	     (assoc key-1
+		    (cdr local-table))))
+	(if subtable
+	    (let ((record
+		   (assoc key-2
+			  (cdr local-table))))
+	      (if record
+		  (set-cdr! record value)
+		  (set-cdr!
+		   subtable
+		   (cons
+		    (cons key-2 value)
+		    (cdr subtable)))))
+	    (set-cdr!
+	     local-table
+	     (cons
+	      (list key-1 (cons key-2 value))
+	      (cdr local-table)))))
+      'ok)
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+	    ((eq? m 'insert-proc!) insert!)
+	    (else
+	     (error "Unknown operation.."))))
+    dispatch))
+(define operation-table (make-table))
+(define get (operation-table 'lookup-proc))
+(define put (operation-table 'insert-proc!))
+
+;; 3.24
+;; The only change that need is to
+;; change the assoc procedure
+;; to test equality using the
+;; same-key? procedure isted of equal?
+
+(define (make-table same-key?)
+  (let ((local-table (list '*table*)))
+    (define (assoc key records)
+      (cond ((null? records) #f)
+	    ((same-key? key (caar records))
+	     car records)
+	    (else (assoc key (cdr records)))))
+    (define (lookup key-1 key-2)
+      (let ((subtable
+	     (assoc key-1 (cdr local-table))))
+	(if subtable
+	    (let ((record
+		   (assoc key-2 (cdr subtable))))
+	      (if record (cdr record) #f)))))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable
+	     (assoc key-1 (cdr local-table))))
+	(if subtable
+	    (let ((record
+		   (assoc key-2 (cdr subtable))))
+	      (if record
+		  (set-cdr! record value)
+		  (set-cdr! subtable
+			    (cons (cons key-2 value)
+				  (cdr subtable)))))
+	    (set-cdr!
+	     local-table
+	     (cons (list key-1 (cons key-2 value))
+		   (cdr local-table)))))
+      'ok)
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+	    ((eq? m 'insert-proc!) insert!)
+	    (else (error "Invalid operation"))))
+    dispatch))
