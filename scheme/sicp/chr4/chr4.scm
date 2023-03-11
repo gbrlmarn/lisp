238b773e86d7851080b62adbdddefc0ba2c0c36b
Gabriel Marin <gbrlmarn@proton.me>
2023-03-11 22:28:08 +0200

    4.21

--- scheme/sicp/chr4/chr4.scm
+++ scheme/sicp/chr4/chr4.scm
@@ -676,3 +676,35 @@
 		   (cons (car vals) new-vals)))))
     (scan vars vals '() '())))
 
+;; 4.21: Amanzingly, Louis's intiition in Exercise 4.20 is correct. It is indeed possible to specify recursive procedures without using letrc (or even define), althought the method for accomplishing this is much more subtle than Louis imagined. The following expressino coputes 10 factorial by applying a recursive factorial procedure:
+((lambda (n)
+   ((lambda (fact) (fact fact n))
+    (lambda (ft k) (if (= k 1)
+		       1
+		       (* k (ft ft (- k 1)))))))
+ 10)
+
+;; a. Check (by evaluating the expression) that this really does computes factorials. Devise an analogous expression for computing Fibonacci numbers.
+((lambda (n)
+   ((lambda (fib) (fib fib n))
+    (lambda (ft k) (cond ((= k 0) 1)
+			 ((= k 1) 1)
+			 (else
+			  (+ (ft ft (- k 1))
+			     (ft ft (- k 2))))))))
+ 10)
+
+;; b. Consider the following procedure, which includes mutually recursive internal definitions:
+(define (f x)
+  (define (even? n)
+    (if (= n 0) true (odd? (- n 1))))
+  (define (odd? n)
+    (if (= n 0) false (even? (- n 1))))
+  (even? x))
+;; Fill in the missing expressions to complete an alternative definition of f, which uses neighter internal definitions nor letrec:
+(define (f x)
+  ((lambda (even? odd?) (even? even? odd? x))
+   (lambda (ev? od? n)
+     (if (= n 0) true (ev? od? (- n 1))))
+   (lambda (ev? od? n)
+     (if (= n 0) false (ev? od?am (- n 1))))))
