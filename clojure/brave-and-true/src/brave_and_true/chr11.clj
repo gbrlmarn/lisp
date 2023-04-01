f464025fa690e7ff7545dbefdfe846e97b410686
Gabriel Marin <gbrlmarn@gmail.com>
2023-04-01 23:25:10 +0300

    more channels and alts!!

--- clojure/brave-and-true/src/brave_and_true/chr11.clj
+++ clojure/brave-and-true/src/brave_and_true/chr11.clj
@@ -38,3 +38,117 @@
 (let [t (thread "chilli")]
   (<!! t))
 
+;; Dog Machine Process You've Been Longing For
+(defn hot-dog-machine
+  []
+  (let [in (chan)
+        out (chan)]
+    (go (<! in)
+        (>! out "hot dog"))
+    [in out]))
+
+(let [[in out] (hot-dog-machine)]
+  (>!! in "pocket lint")
+  (<!! out))
+
+;; Limited hot dogs in the machine
+(defn hot-dog-machine-v2
+  [hot-dog-count]
+  (let [in (chan)
+        out (chan)]
+    (go (loop [hc hot-dog-count]
+          (if (> hc 0)
+            (let [input (<! in)]
+              (if (= 3 input)
+                (do (>! out "hot dog")
+                    (recur (dec hc)))
+                (do (>! out "wilted lettuce")
+                    (recur hc))))
+            (do (close! in)
+                (close! out)))))
+    [in out]))
+
+(let [[in out] (hot-dog-machine-v2 2)]
+  (>!! in "pocket lint")
+  (println (<!! out))
+
+  (>!! in 3)
+  (println (<!! out))
+
+  (>!! in 3)
+  (println (<!! out))
+
+  (>!! in 3)
+  (println (<!! out)))
+
+
+;; Transformations using multiple chanels
+(let [c1 (chan)
+      c2 (chan)
+      c3 (chan)]
+  (go (>! c2 (clojure.string/upper-case (<! c1))))
+  (go (>! c3 (clojure.string/reverse (<! c2))))
+  (go (println (<! c3)))
+  (>!! c1 "redrum"))
+
+;; alts!!
+(defn upload
+  [headshot c]
+  (go (Thread/sleep (rand 100))
+      (>! c headshot)))
+
+(let [c1 (chan)
+      c2 (chan)
+      c3 (chan)]
+  (upload "serious.jpg" c1)
+  (upload "fun.jpg" c2)
+  (upload "sassy.jpg" c3)
+  (let [[headshot channel] (alts!! [c1 c2 c3])]
+    (println "Sending headshot notification for"
+            headshot)))
+;; With the help of 'alts!!' the first result that appear on any channle is spit out :D
+;; Also 'alts!!' permits to put a timeout. If the timeout is reached, 'alts!!' terminate
+(let [c1 (chan)]
+  (upload "serious.jpg" c1)
+  (let [[headshot channel]
+        (alts!! [c1 (timeout 20)])]
+    (if headshot
+      (println "Sending headshot notification for" headshot)
+      (println "Time out!"))))
+
+;;
+(let [c1 (chan)
+      c2 (chan)]
+  (go (<! c2))
+  (let [[value channel]
+        (alts!! [c1 [c2 "put!"]])]
+    (println value)
+    (= channel c2)))
+
+;; 'alts!!' is blocking while 'alts!' is parking
+(defn append-to-file
+  "Write a string to the end of a file"
+  [filename s]
+  (spit filename s :append true))
+
+(defn format-quote
+  "Delineate the beginning and end of a quote because it's convenient"
+  [quote]
+  (str "=== BEGIN QUOTE === \n"
+       quote
+       "=== END QUOTE \n\n"))
+
+(defn random-quote
+  "Retrive random quote and format it"
+  []
+  (format-quote
+   (slurp "https://www.braveclojure.com/random-quote" )))
+
+(defn snag-quotes
+  [filename num-quotes]
+  (let [c (chan)]
+    (go
+      (while true
+        (append-to-file filename (<! c))))
+    (dotimes [n num-quotes]
+      (go (>! c (random-quote))))))
