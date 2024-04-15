2e89209fe0fa30a6cf466e49135d0d8ce093520f
Gabriel Marin <marin.gabriel@protonmail.com>
2024-04-16 00:18:55 +0300

    Managed to include assets in ring server such as a pdf file

--- clojure/example-static-website/project.clj
+++ clojure/example-static-website/project.clj
@@ -1,13 +1,14 @@
 (defproject example-static-website "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Code that generates marinmgabriel.github.io"
+  :url "https://marinmgabriel.github.io"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [stasis "2.5.0"]
                  [markdown-clj "1.10.5"]
                  [hiccup "1.0.5"]
-                 [ring "1.8.2"]]
+                 [ring "1.8.2"]
+                 [optimus "2023.11.21"]]
   :ring {:handler example-static-website.core/server}
   :profiles {:dev {:plugins [[lein-ring "0.12.5"]]}}
   :repl-options {:init-ns example-static-website.core}
--- clojure/example-static-website/src/example_static_website/core.clj
+++ clojure/example-static-website/src/example_static_website/core.clj
@@ -2,12 +2,20 @@
   (:require [clojure.string :as str]
             [stasis.core :as stasis]
             [markdown.core :as md]
-            [hiccup.page :as hiccup]))
+            [hiccup.page :as hiccup]
+            [optimus.assets :as assets]
+            [optimus.optimizations :as optimizations]
+            [optimus.strategies :refer [serve-live-assets]]
+            [optimus.prime :as optimus]
+            [optimus.export]
+            [ring.middleware.content-type :refer [wrap-content-type]]))
 
 ;; vars
 (def source-dir "resources")
 
 ;; functions
+(defn get-assets []
+  (assets/load-assets "public" ["/marincv.pdf"]))
 
 (defn key-to-html [s]
   (str/replace s #".md" ".html"))
@@ -27,14 +35,16 @@
                  [:meta {:name "viewport"
                          :content "width=device-width, initial-scale=1.0"}]
                  [:link {:type "text/css"
-                         :href "/css/style.css"
+                         :href "public/css/style.css"
                          :rel "stylesheet"}]
                  [:body
                   [:div {:class "header"}
                    [:div {:class "name"}
                     [:a {:href "/"} "Home page"]
                     [:div {:class "header-right"}
-                     [:a {:href "/posts"} "Posts"]]]]
+                     [:a {:href "/posts"} "Posts"]]
+                    [:div {:class "header-right"}
+                     [:a {:href "/marincv.pdf"} "Curriculum Vitae"]]]]
                   page]
                  [:footer
                   [:p "This is the footer"]]]))
@@ -56,27 +66,29 @@
 ;; end of functions
 
 ;; tests
-
 (md/md-to-html-string "# This should be h1")
 (md/md-to-html-string "## This should be h2")
 (stasis/slurp-directory source-dir #".*\.md$")
-(keys (stasis/slurp-directory source-dir
-                              #".*\.md$"))
+(keys (stasis/slurp-directory source-dir #".*\.md$"))
 (format-pages (read-and-convert! source-dir))
 (apply-header-footer "here is some content!")
 (read-and-convert! source-dir)
 (str/replace "index.md" #".md" ".html")
 
 ;; end of tests
+(def optimize optimizations/all)
 
-(def server
-  (stasis/serve-pages
-   (merge-website-assets! source-dir)))
+(def server (-> (stasis/serve-pages
+                 (merge-website-assets! source-dir))
+                (optimus/wrap get-assets optimize serve-live-assets)
+                wrap-content-type))
 
 (def out-dir "docs")
 
 (defn export! []
-  (stasis/empty-directory! out-dir)
-  (stasis/export-pages
-   (merge-website-assets! source-dir) out-dir)
-  (println "Website is ready!"))
+  (let [assets (optimize (get-assets) {})]
+    (stasis/empty-directory! out-dir)
+    (optimus.export/save-assets assets out-dir)
+    (stasis/export-pages
+     (merge-website-assets! source-dir) out-dir {:optimus-assets assets})
+    (println "Export complete")))
